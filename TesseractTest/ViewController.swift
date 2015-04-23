5a7b507ca29a452fd5659764471a2d66b6d465e6
田中賢治 <key.m4096.kung.fu@gmail.com>
2015-04-23 14:46:05 +0900

    認識させる画像を白黒にすることで認識率を向上させた

--- TesseractTest/ViewController.swift
+++ TesseractTest/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, G8TesseractDelegate {
     var imageView: UIImageView = UIImageView()
     var label: UILabel = UILabel()
     
+    var adjustedImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,6 +30,24 @@ class ViewController: UIViewController, G8TesseractDelegate {
         self.view.addSubview(imageView)
         self.view.addSubview(label)
         
+        var image = UIImage(named: "OCR-Sample-Japanese")
+        
+        //文字を読みやすくするため、白黒にする
+        var ciImage = CIImage(image: image)
+        
+        var ciFilter = CIFilter(name: "CIColorMonochrome")
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        ciFilter.setValue(CIColor(red: 0.75, green: 0.75, blue: 0.75), forKey: kCIInputColorKey)
+        ciFilter.setValue(NSNumber(float: 1.0), forKey: kCIInputIntensityKey)
+        
+        var ciContext = CIContext(options: nil)
+        var cgImage: CGImageRef = ciContext.createCGImage(ciFilter.outputImage, fromRect: ciFilter.outputImage.extent())
+        
+        // 文字解析対象の画像の色、コントラストを調整したものを変数に保存する
+        adjustedImage = UIImage(CGImage: cgImage)
+        
+        imageView.image = adjustedImage
+        
         analyze()
     }
     
@@ -37,11 +57,9 @@ class ViewController: UIViewController, G8TesseractDelegate {
     
     func analyze() {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {
-            var image = UIImage(named: "OCR-Sample-Japanese")
-            
             var tesseract = G8Tesseract(language: "jpn")
             tesseract.delegate = self
-            tesseract.image = image
+            tesseract.image = self.adjustedImage
             tesseract.recognize()
         
             self.label.text = tesseract.recognizedText
